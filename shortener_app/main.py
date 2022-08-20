4e8b05327c20dc47f59358abfc053b8adf77da5d
Mukthy <muktheeswaran.m@gmail.com>
2022-08-20 20:35:39 +0530

    Refactoring code

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -44,9 +44,9 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
 
 @app.get("/{url_key}")
 def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
-    db_url = (db.query(models.URL).filter(models.URL.key == url_key, models.URL.is_active).first())
+    
 
-    if db_url:
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
