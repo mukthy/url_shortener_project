7dafd3391e58ff01b78b532e9b7743e3e89b772b
Mukthy <muktheeswaran.m@gmail.com>
2022-08-23 06:02:22 +0530

    Modified the main.py for admin endpoint

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 import validators
 from . import schemas, models, crud
 from .database import SessionLocal, engine
+from starlette.datastructures import URL
+from .config import get_settings
 
 
 app = FastAPI()
@@ -28,18 +30,24 @@ def read_root():
     return "Welcome to the URL shortener API"
 
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "Administration Info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
+
 @app.post("/url")
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         return raise_bad_request(message="The URL you have provided is not valid")
     # return f"TODO: Create database entry for {url.target_url}"
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-    db_url.url = key
-    db_url.admin_url = secret_key
-
-    return db_url
+    
+    return get_admin_info(db_url)
 
 
 @app.get("/{url_key}")
@@ -48,5 +56,15 @@ def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(
 
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get("/admin/{secret_key}", name="Administration Info", response_model=schemas.URLInfo,)
+def get_url_info(
+        secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
